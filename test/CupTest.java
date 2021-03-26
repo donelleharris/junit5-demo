67a39c1c5295ebf642d2328aeaddefcf23ae0fce
Donelle Harris <donelle.harris@gmail.com>
2021-03-26 08:09:13 -0500

    Continue JUnit5 testing CupTest

--- test/CupTest.java
+++ test/CupTest.java
@@ -1,4 +1,4 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +8,25 @@ class CupTest {
     void getLiquidType() {
         Cup c = new Cup("Orange Juice", 85.5);
         assertEquals("Orange Juice", c.getLiquidType());
+    }
+    @Test
+    void getPercentFull() {
+        Cup c = new Cup("Orange Juice", 85.5);
+        assertEquals(85.5, c.getPercentFull(), 0.001);
+    }
+
+
+    @Test
+    void setLiquidType() {
+        Cup c = new Cup("Orange Juice", 85.5);
+        c.setLiquidType("water");
+        assertEquals("water", c.getLiquidType());
+    }
+
+    @Test
+    void setPercentFull() {
+        Cup c = new Cup("Orange Juice", 85.5);
+        c.setPercentFull(90.5);
+        assertEquals(90.5, c.getPercentFull(), 0.001);
     }
 }
